61e0e1a51384ac39273b6cab58fbf2bf095407ae
Arianna Avalos <arianna.avalos15@gmail.com>
2023-01-20 16:33:55 -0500

    init

--- dist/user/user.controller.js
+++ dist/user/user.controller.js
@@ -5,21 +5,25 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppModule = void 0;
-const common_1 = require("@nestjs/common");
-const app_controller_1 = require("./app.controller");
-const app_service_1 = require("./app.service");
-const user_controller_1 = require("./user/user.controller");
-const user_module_1 = require("./user/user.module");
-let AppModule = class AppModule {
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-AppModule = __decorate([
-    (0, common_1.Module)({
-        imports: [user_module_1.UserModule],
-        controllers: [app_controller_1.AppController, user_controller_1.UserController],
-        providers: [app_service_1.AppService],
-    })
-], AppModule);
-exports.AppModule = AppModule;
-//# sourceMappingURL=app.module.js.map
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserController = void 0;
+const common_1 = require("@nestjs/common");
+let UserController = class UserController {
+    getUsers() {
+        return "retrieving users";
+    }
+};
+__decorate([
+    (0, common_1.Get)(),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", void 0)
+], UserController.prototype, "getUsers", null);
+UserController = __decorate([
+    (0, common_1.Controller)('user')
+], UserController);
+exports.UserController = UserController;
+//# sourceMappingURL=user.controller.js.map
