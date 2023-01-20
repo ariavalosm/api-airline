09ed37e9b0325591beb3d0d667ae4fc98b53ad97
Arianna Avalos <arianna.avalos15@gmail.com>
2023-01-20 17:31:56 -0500

    init

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get , Post , Put , Delete, Body } from '@nestjs/common';
+import { Controller, Get , Post , Put , Delete, Body, Param } from '@nestjs/common';
 
 import { CreateUserDto } from './dto/create-user.dto';
 
@@ -15,14 +15,16 @@ export class UserController {
     return "hello";
   }
 
+  @Delete(":id")
+  deleteUsers(@Param("id") id): string{
+    console.log(id);
+  return `deleting users: ${id}`;
+  }
 
   @Put()
   updateUsers(): string{
     return "invocando users";
   }
 
-  @Delete()
-  deleteUsers(): string{
-    return "retrieving users";
-  }
+  
 }
