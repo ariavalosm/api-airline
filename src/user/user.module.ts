a7e830455e29ed71b2afbede3c340f9dcbcd5846
Arianna Avalos <arianna.avalos15@gmail.com>
2023-01-23 17:19:43 -0500

    added

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UsersService } from './user.service';
 import { MongooseModule } from "@nestjs/mongoose";
-import {} from "./schema/user.schema";
+import { DatosSchema } from "./schema/user.schema";
 
 @Module({
   imports: [ MongooseModule.forFeature([
-    { name: "Datos" , schema: DatosSchema}
+    { name: "Usuarios" , schema: DatosSchema}
   ])],
   controllers: [UserController],
   providers: [UsersService]
