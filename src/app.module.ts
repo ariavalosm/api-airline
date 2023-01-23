6c67aa8d9e2cb4f9f222cca943814f83873c3b4e
Arianna Avalos <arianna.avalos15@gmail.com>
2023-01-23 16:16:27 -0500

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { UsersService } from './user/user.service';
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, MongooseModule ],
   controllers: [AppController, UserController],
   providers: [AppService, UsersService],
 })
