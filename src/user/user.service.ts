e57e9f5054ad9cd4efa6cdb060ae0ec9a5d0f070
Arianna Avalos <arianna.avalos15@gmail.com>
2023-01-23 17:21:01 -0500

    added

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,38 +5,6 @@ import { user } from "./interfaces/user"
 export class UsersService {
 
   users : user[] = [
-    {
-      id: 1,
-      name: "Arianna",
-      lastname: "Avalos",
-      nationality: "Peruana",
-      doctype: "DNI",
-      docnumber: 1234567
-    },
-    {
-      id: 2,
-      name: "Arianna",
-      lastname: "Avalos",
-      nationality: "Peruana",
-      doctype: "DNI",
-      docnumber: 1234567
-    },
-    {
-      id: 3,
-      name: "Arianna",
-      lastname: "Avalos",
-      nationality: "Peruana",
-      doctype: "DNI",
-      docnumber: 1234567
-    },
-  ];
-
-  getUsers() :user[]{
-    return this.users;
-  }
-
-  getUser(id: number) :user{
-    return this.users.find(user => user.id === id);
-
-  }
+    
+  ]
 }
